eb8e780ab1723fbaa057eab49e9a990339d34a9d
kefitaib <taibkefi@yahoo.fr>
2020-04-14 15:47:10 -0700

    fix 1.3

--- tools.c
+++ tools.c
@@ -11,7 +11,7 @@
 
 int test_built_in(char *buffer, char **env, his **head)
 {
-	int i = 0, test = 0;
+	int /* i = 0,  */test = 0;
 	char *tmp = NULL;
 
 	tmp = strip_built_in(buffer);
@@ -21,15 +21,6 @@ int test_built_in(char *buffer, char **env, his **head)
 		if (_strcmp("exit", tmp) == 0)
 			test = 1;
 
-		if (_strcmp("env", tmp) == 0)
-		{
-			for (i = 0; env[i]; i++)
-			{
-				_print(env[i]);
-				_print("\n");
-			}
-			test = 2;
-		}
 		if (_strcmp("history", tmp) == 0)
 		{
 			_print_history(head, env);
